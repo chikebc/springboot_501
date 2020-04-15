cbcadcefb0cfd0b9852836d040d715fe4fb51ee6
chikebc <chikecb@outlook.com>
2020-04-15 15:37:50 -0400

    Initial commit

---
+++ src/test/java/com/springboot501/springboot_501/Springboot501ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot501.springboot_501;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springboot501ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
